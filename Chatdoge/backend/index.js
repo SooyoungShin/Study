0fa69447e66551983e0286d358184d30b33da100
Sooyoung <gszod12@gmail.com>
2023-09-17 14:16:54 -0400

    update

--- Chatdoge/backend/index.js
+++ Chatdoge/backend/index.js
@@ -1,1 +1,16 @@
-console.log("hello world!")
+const OpenAI = require('openai')
+
+const openai = new OpenAI({
+  apiKey: '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', // defaults to process.env["OPENAI_API_KEY"]
+});
+
+async function main() {
+  const completion = await openai.chat.completions.create({
+    messages: [{ role: 'user', content: 'Say this is a test' }],
+    model: 'gpt-3.5-turbo',
+  });
+  return completion
+  console.log(completion.choices);
+}
+
+main();
